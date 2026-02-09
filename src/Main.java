917b5cb8d7daef0a82ebf1915679766c6d395347
Diego Lizarraga <lizarragasanchezdiego@gmail.com>
2026-02-09 14:52:17 -0600

    se parece a c++ pero con mas texto mano

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hola mundo por 5ta vez");
+        System.out.println("Pelicula: 2 tipos de cuidado");
+        /* la del moño colorado jsjsjsjsj
+        aprendio a usar comentarios se nota
+         */
+        int fechaDeLanzamiento = 1950;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
 
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);
+        //pues es como c++ esto de los comentarios.
+        String sinopsis = """
+               pura pelicula ranchera 
+               aguas que balacean personas en la peli
+               nose que mas decir
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
     }
 }
