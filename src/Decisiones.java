68f7ca7e5edeb54b9c284fc591c50c1c1492f927
Diego Lizarraga <lizarragasanchezdiego@gmail.com>
2026-02-26 13:15:22 -0600

    nueva cosas que se olvido subir de alura dx

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -1,16 +1,21 @@
 public class Decisiones {
-    public static void main(String[] args){
-        int fechaDeLanazamiento = 2023;
+
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
-
-        if(fechaDeLanazamiento > 2022){
-            System.out.println("Peliculas mas populares");
-        }else{
-            System.out.println("Pelicula retro que pues lleva rato");
+        String tipoPlan = "plus";
+// código que prueba los operadores relacionales
+        if (fechaDeLanzamiento >= 2022){
+            System.out.println("Péliculas más populares");
+        }else {
+            System.out.println("Película Retro que aún vale la pena ver");
         }
 
-
+        if (incluidoEnElPlan && tipoPlan.equals("plus")){
+            System.out.println("Disfrute de su película");
+        } else {
+            System.out.println("Película no disponible para su plan actual");
+        }
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hola mundo por 5ta vez");
-        System.out.println("Pelicula: 2 tipos de cuidado");
-        /* la del moño colorado jsjsjsjsj
-        aprendio a usar comentarios se nota
-         */
-        int fechaDeLanzamiento = 1950;
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Película: Matrix");
+
+        int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
 
         double media = (8.2 + 6.0 + 9.0) / 3;
         System.out.println(media);
-        //pues es como c++ esto de los comentarios.
-        String sinopsis = """
-                pura pelicula ranchera 
-                aguas que balacean personas en la peli
-                nose que mas decir
-                """ + fechaDeLanzamiento;
+
+       String sinopsis = """
+               Matrix es una paradoja
+               La mejor película del fin del milenio
+               Fué lanzada en: 
+               """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
 
         int clasificacion = (int) (media / 2);
-
         System.out.println(clasificacion);
 
 
 
-
-    /*
-        // Variable en Celsius
-        double celsius = 25.0;
-
-        // Conversión a Fahrenheit usando la fórmula
-        double fahrenheit = (celsius * 1.8) + 32;
-
-        // Mostrar resultado con decimales
-        System.out.println(celsius + " grados Celsius son " + fahrenheit + " grados Fahrenheit.");
-
-        // Casting a entero para quitar decimales
-        int fahrenheitEntero = (int) fahrenheit;
-
-        // Mostrar resultado sin decimales
-        System.out.println("Temperatura en Fahrenheit (sin decimales): " + fahrenheitEntero);
-    */
     }
 }
